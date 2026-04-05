720457684c7c13d2e0acd645422127babd7323db
Loru <loru@foxmail.com>
2026-04-05 14:24:53 +0800

    fix: load live school system in Android app

--- android/app/src/main/java/com/loru/schoolsystem/MainActivity.kt
+++ android/app/src/main/java/com/loru/schoolsystem/MainActivity.kt
@@ -1,32 +1,5 @@
 package com.loru.schoolsystem
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.loru.schoolsystem.ui.components.AdaptiveApp
-import com.loru.schoolsystem.ui.theme.SchoolSystemTheme
+import com.getcapacitor.BridgeActivity
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
-        setContent {
-            SchoolSystemTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    AdaptiveApp()
-                }
-            }
-        }
-    }
-}
+class MainActivity : BridgeActivity()
