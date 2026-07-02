b4610cc5e10a9d9c6080eb4cf0b954059421e875
Loru <hkakjiweu@gmail.com>
2026-07-02 08:57:14 +0800

    Revert "Remount assessment sync after module switch"
    This reverts commit 73071a3862356b24c38e730367c865e9a45ad462.

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@
  * fallbacks when the network is unavailable.
  */
 
-const CACHE_VERSION = 'school-system-runtime-cdb9679b972d';
+const CACHE_VERSION = 'school-system-runtime-c1dcaa2e19ef';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const API_CACHE = `${CACHE_VERSION}-api`;
