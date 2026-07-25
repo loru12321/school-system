9c7dbbf5a6412e32123721cca2b5880dc715ebea
Loru <hkakjiweu@gmail.com>
2026-07-25 09:40:33 +0800

    style: unify login and new Chinese workbench

--- public/sw-runtime-16c0585474cd.js
+++ public/sw-runtime-16c0585474cd.js
@@ -4,7 +4,7 @@
  * fallbacks when the network is unavailable.
  */
 
-const CACHE_VERSION = 'school-system-runtime-d1d27e612815';
+const CACHE_VERSION = 'school-system-runtime-16c0585474cd';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const API_CACHE = `${CACHE_VERSION}-api`;
