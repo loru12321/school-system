3f32d1630b9119e7883fc2614dc89228bb27e715
Loru <hkakjiweu@gmail.com>
2026-08-28 12:26:56 +0800

    build: refresh subject balance release assets

--- dist/sw.js
+++ dist/sw.js
@@ -4,7 +4,7 @@
  * fallbacks when the network is unavailable.
  */
 
-const CACHE_VERSION = 'school-system-runtime-c82e163522f8';
+const CACHE_VERSION = 'school-system-runtime-704aa020d906';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const API_CACHE = `${CACHE_VERSION}-api`;
