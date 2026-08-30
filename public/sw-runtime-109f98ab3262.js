2e0a0238cd663c4748e46b3c169fe1d14b9c58a6
Loru <hkakjiweu@gmail.com>
2026-08-30 08:24:10 +0800

    build: refresh versioned service worker runtime

--- public/sw-runtime-109f98ab3262.js
+++ public/sw-runtime-109f98ab3262.js
@@ -4,7 +4,7 @@
  * fallbacks when the network is unavailable.
  */
 
-const CACHE_VERSION = 'school-system-runtime-0485a64a8693';
+const CACHE_VERSION = 'school-system-runtime-109f98ab3262';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const API_CACHE = `${CACHE_VERSION}-api`;
